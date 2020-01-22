90a97113020e815e2163b196810a82eec3033c18
Unknown <jkm.svensson@gmail.com>
2020-01-22 10:24:38 +0100

    Added XZING

---
+++ Rohsska/Assets/Scripts/Main/BarCodeReader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using GoogleARCore;
+
+public class BarCodeReader : MonoBehaviour
+{
+    public BarCodeReader barCodeReader;
+
+}
