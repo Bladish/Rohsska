44f81cee527f84b4a5fda343f557f0e8decb30cb
Unknown <jkm.svensson@gmail.com>
2020-01-24 13:43:07 +0100

    Imagetracking working

---
+++ Rohsska/Assets/Scripts/Main/AugmentedImageSwapScene.cs
@@ -0,0 +1,24 @@
+﻿namespace AugmentedImageSwapScene
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+
+
+    public class AugmentedImageSwapScene : MonoBehaviour
+    {
+        public AugmentedImage Image;
+
+        public void Update()
+        {
+            if (Image == null || Image.TrackingState != TrackingState.Tracking) return;
+
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
+}
