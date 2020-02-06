9cdd3ecff1d4b90a657c0ea9922dbaba5ba434d2
Unknown <jkm.svensson@gmail.com>
2020-02-06 15:35:05 +0100

    Raycast

---
+++ Rohsska/Assets/Scripts/Controllers/RayCastController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+//RayCastController for Unity physics.
+public class RayCastController : MonoBehaviour
+{
+    public GameObject testingObjectRed;
+    public GameObject testingObjectBlack;
+    
+
+    void Update()
+    {
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                RaycastHit hit;
+                int layerMask = 1 << 8;
+                if (Physics.Raycast(ray,out hit, Mathf.Infinity, layerMask))
+                {
+                    //If layermask is intericable you can do stuff to the object here.
+                    
+                }
+            }
+        }
+        
+    }
+}
