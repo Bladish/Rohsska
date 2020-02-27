28bfce39b727d753f0e17b4c24b1824ee6b8449f
Unknown <jkm.svensson@gmail.com>
2020-02-27 08:43:24 +0100

    ARFundation

---
+++ Rohsska/Assets/Scripts/Controllers/RayCastController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+//RayCastController for Unity physics.
+public class RayCastController : MonoBehaviour
+{
+    public GameObject AnchorObjet;
+    public GameObject orignalObject;
+    public GameObject sweObject;
+    public GameObject engObject;
+    public GameObject buttonOne;
+    public GameObject buttonTwo;
+
+    private void Start()
+    {
+        sweObject.SetActive(false);
+        engObject.SetActive(false);
+    }
+    void Update()
+    {
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                RaycastHit hit;
+                int layerMask = 1 << 8;
+                if (Physics.Raycast(ray,out hit, Mathf.Infinity, layerMask))
+                {
+                    if (hit.transform.name == "China Button")
+                    {
+                        orignalObject.SetActive(true);
+                        sweObject.SetActive(false);
+                    }
+
+                    if (hit.transform.name == "Sweden Button")
+                    {
+                        orignalObject.SetActive(false);
+                        sweObject.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Rohsska/Assets/Scripts/Main/ImageRecognition.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using TMPro;
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+    public GameObject FitToScanOverlay;
+    
+    void Awake()
+    {
+        _arTrackedImageManager = GetComponent<ARTrackedImageManager>();
+        Application.targetFrameRate = 60;
+        FitToScanOverlay.SetActive(true);
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            switch (trackedImage.referenceImage.name)
+            {
+                case "000":
+                    SceneManager.LoadScene("Scroll");
+                    FitToScanOverlay.SetActive(false);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
