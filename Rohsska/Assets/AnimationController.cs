2a98126b85ee00c7f2795a373757fd13ed88e96c
Unknown <jkm.svensson@gmail.com>
2020-04-20 11:36:37 +0200

    Don't know what this is

---
+++ Rohsska/Assets/AnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    
+    public static Animator animator;
+    public static bool runAnimation;
+
+    private void Update()
+    {
+        if (runAnimation) 
+        {
+            animator.Play("ScrollUpDown");
+        } 
+    }
+}
