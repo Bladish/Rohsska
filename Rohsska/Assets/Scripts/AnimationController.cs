8874a010e7e97be5ae5c3c992db100e96b877f18
Unknown <jkm.svensson@gmail.com>
2020-04-23 12:55:16 +0200

    Next test

--- Rohsska/Assets/Scripts/AnimationController.cs
+++ Rohsska/Assets/Scripts/AnimationController.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 public class AnimationController : MonoBehaviour
 {
     public new Animation animation;
-    public Text text;
+    public Animator animator;
 
     public void PlayAnimation ()
     {
-        text.text = "Triggered";
-        animation.Play("ScrollUpDown");
+        animator.Play("ScrollUpDown");
     }
 }
