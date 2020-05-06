92ea5ca5bc4ea8db53a98a4c76f48854afaa68f6
Menoka <sebastian_kba@hotmail.com>
2020-05-06 14:14:37 +0200

    Fish animation and particle interaction

---
+++ Rohsska/Assets/Scripts/FishAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAnimationController : MonoBehaviour
+{
+    Animator fishAnimator;
+    [SerializeField]
+    ParticleSystem waterSplash;
+    bool canRunAnimation = true;
+    int startChance = 50;
+    void Start()
+    {
+        fishAnimator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (canRunAnimation)
+        {
+            fishAnimator.Play("NewFishJump");
+            canRunAnimation = false;
+        }
+    }
+    public void SplashParticles()
+    {
+        waterSplash.Play();
+        canRunAnimation = true;
+    }
+}
