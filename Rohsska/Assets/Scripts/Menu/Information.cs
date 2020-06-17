f0c3aab11109d32bfd6b99e5d812e01c80a87940
Patrik <pajo0043@gmail.com>
2020-06-17 13:05:23 +0200

    Edits meny

---
+++ Rohsska/Assets/Scripts/Menu/Information.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Information : MonoBehaviour
+{
+    public GameObject information;
+
+
+    public void SetUIInformation ()
+    {
+        if (information.activeInHierarchy == false)
+        {
+            information.SetActive(true);
+        }
+        else 
+        {
+            information.SetActive(false);
+        }
+    }
+
+
+}
